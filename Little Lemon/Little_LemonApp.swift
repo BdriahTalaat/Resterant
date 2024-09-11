c665cec4f62fe3e377377ac7baf4e771d0abbf06
BdriahFakhrji <bdriahtalaat@gmail.com>
2024-09-11 08:15:16 +0300

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Bdriah Talaat on 08/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
